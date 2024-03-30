f08df587c3a05a1e380653cdc9a5a224682a1985
Brenda Matias <brendamatias.sobral@gmail.com>
2024-03-30 18:59:59 -0300

    feat: add signin and singup screens

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import { LogBox, StatusBar, View } from 'react-native';
 import { NativeBaseProvider } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { useEffect } from 'react';
-import Loading from '@components/Loading';
 import { THEME } from './src/theme';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Loading /> : <View />}
+      {fontsLoaded ? <SignUp /> : <View />}
     </NativeBaseProvider>
   );
 }
