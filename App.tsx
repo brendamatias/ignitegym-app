ace3d0e198c605d93f19f83aeab278e3dbe81a48
Brenda Matias <brendamatias.sobral@gmail.com>
2024-03-30 18:06:29 -0300

    initial commit
    Generated by create-expo-app 2.1.1.

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { LogBox, StatusBar, View } from 'react-native';
+import { NativeBaseProvider } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { useEffect } from 'react';
+import Loading from '@components/Loading';
+import { THEME } from './src/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  useEffect(() => {
+    LogBox.ignoreLogs(['In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app.']);
+  }, []);
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Loading /> : <View />}
+    </NativeBaseProvider>
+  );
+}
